1debac477dbb9750e12ba10ba8d68f153c33a316
Vahe <67160168+vahe-arzumanyan@users.noreply.github.com>
2023-02-10 00:34:12 +0400

    assas12321d

--- src/shopping_page/component/footer/index.jsx
+++ src/shopping_page/component/footer/index.jsx
@@ -2,17 +2,20 @@ import React from "react";
 import './style.scss';
 
 const Footer = () => {
-    return <div className='G-container'>
-        <div className='G-flex G-justify-between P-footer'>
-            <div className='G-flex-column'>
+    return <div className='P-footer'>
+        <div className='G-flex G-justify-between G-container'>
+            <div className='G-flex-column P-footer-contact'>
                 <p className='P-footer-info-title'>GET IN TOUCH</p>
-                <p>No dolore ipsum accusam no lorem. Invidunt sed clita kasd clita et et dolor sed dolor. Rebum
+                <p className='P-footer-info-description'>No dolore ipsum accusam no lorem. Invidunt sed clita kasd clita
+                    et et dolor sed dolor. Rebum
                     tempor no vero est magna amet no</p>
-                <p>123 Street, New York, USA</p>
-                <p>test@gmail.com</p>
-                <p>+010 1010 01010</p>
+               <div className='P-footer-info-address'>
+                   <p>123 Street, New York, USA</p>
+                   <p>test@gmail.com</p>
+                   <p>+ 010 1010 01010</p>
+               </div>
             </div>
-            <div className='G-flex-column'>
+            <div className='G-flex-column P-footer-quick-shop'>
                 <p>QUICK SHOP</p>
                 <ul className='G-flex G-flex-column'>
                     <li> > Home</li>
@@ -24,10 +27,8 @@ const Footer = () => {
                     <li> > Contact Us</li>
                 </ul>
             </div>
-            <div className='G-flex-column'>
-
+            <div className='G-flex-column P-footer-my-account'>
                 <p>MY ACCOUNT</p>
-
                 <ul className='G-flex-column'>
                     <li> > Home</li>
                     <li> > Our Shop</li>
